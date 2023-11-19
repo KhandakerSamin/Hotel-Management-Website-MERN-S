5926ef74cfa8f05e933ab3c04e0c98ed72cb2d7f
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2023-11-19 09:08:44 +0600

    users api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vheow1k.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,12 +27,116 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        // ? All Colections : 
 
+        const userCollection = client.db('BristoBossDB').collection('users');
         const menuCollection = client.db('BristoBossDB').collection('menu');
+        const reviewCollection = client.db('BristoBossDB').collection('reviews');
+        const cartCollection = client.db('BristoBossDB').collection('carts');
 
 
-        app.get('/menu', async(req, res) => {
+
+
+        // ! jwt related api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ token });
+        })
+
+
+        // ! middlewares 
+        const varifyToken = (req, res, next) => {
+            console.log('inside varify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Unauthorized Access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.varify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                return res.status(401).send({ message: 'Unauthorized Access' })
+                }
+                req.decoded= decoded; 
+                next();
+            })
+
+            // todo next();
+        }
+
+
+
+        // ! users related api 
+
+        app.get('/users', varifyToken, async (req, res) => {
+            console.log(req.headers);
+            const result = await userCollection.find().toArray()
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            //? insert email if user doesent exitst:
+            // todo:  you can do this many ways ( 1. email unique, 2. upsert. simple checking)
+
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', isertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        // ! Menu Related API 
+
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray()
+            res.send(result);
+        })
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
@@ -54,3 +159,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Bistro boss is sitting on port ${port}`);
 })
+
+
+
+/**
+ * -----------------------------------
+ *          Naming Convention
+ * ----------------------------------
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('/users/:id')
+ * app.delete('/uers/:id')
+ * 
+*/
